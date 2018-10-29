59d23035c4cdc0530919e7d686a940de3c8333d4
ahomayouni <arash.homayouni@mail.utoronto.ca>
2018-10-29 16:40:55 -0400

    Added terms of service page with link from/to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'terms_of_service/index'
   get 'about/index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
