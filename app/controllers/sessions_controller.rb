ecf8743604a1a0169392dc59df60fb6929200f02
ptanugra <p.tanugraha@mail.utoronto.ca>
2018-10-29 22:25:05 -0400

    Added login functionality in 'sessions#create'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	# We have downcase here since before saving to database we are downcasing the email address
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		redirect_to user 
+  	else
+  		flash[:danger] = "Invalid email/password combination! Please try again"
+  		render 'new'
+  	end
+
+  end
 end
