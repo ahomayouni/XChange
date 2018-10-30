b171d881b0ed510c48d6b3284d8a5e780682a786
ptanugra <p.tanugraha@mail.utoronto.ca>
2018-10-30 18:10:01 -0400

    hotfix on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,17 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase) #make email string uniform as before_save in db
   	if user && user.authenticate(params[:session][:password])
   		log_in user #call function log_in provided in sessions_helper 
-      remember user #call function remember provided in sessions_helper
+      
+      # Only save the cookies iff the remember me is set to 1
+      # Actually by default it's going to always be "Remember me"
+      # https://stackoverflow.com/questions/6837620/in-rails-how-do-you-destroy-a-session-when-a-browser-is-closed
+      # Cookies setting is working fine though.
+      if params[:session][:remember_me] == '1'
+        remember(user)
+      else
+        forget(user)
+      end
+
   		redirect_to user 
   	else
   		flash.now[:danger] = "Invalid email/password combination! Please try again"
