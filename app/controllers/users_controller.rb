7569c2a8d188ac9d88676ffc41096a3229121125
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-10-29 21:25:43 -0400

    Added sign up page , validation in controllers. Added a flash content manager within the shared views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params_validator)
+  	if @user.save
+  		flash[:success] = "Thanks for signing up with XChange!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	def user_params_validator
+  		params.require(:user).permit(:name, :email,:password,:password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-
-  get 'users/new'
-
   get 'privacy_policy/index'
   get 'terms_of_service/index'
   get 'about/index'
 
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   root 'home#index'
 end
