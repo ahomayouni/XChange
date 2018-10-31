c252627e4b09dfe7f8becfe6783b2ea07c0cc9c5
ptanugra <p.tanugraha@mail.utoronto.ca>
2018-10-30 22:31:35 -0400

    Added Authorization code. Can only go to users#show users#edit users#update if you are logged in. Whitelisting.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show,:edit,:update] # White listing
+
   def new
   	@user = User.new
   end
@@ -35,4 +37,12 @@ class UsersController < ApplicationController
   	def user_params_validator
   		params.require(:user).permit(:name, :email,:password,:password_confirmation)
   	end
+
+    def logged_in_user
+      unless logged_in? 
+        flash[:danger] = "Unauthorized Access. Please log in."
+        redirect_to login_path
+      end
+    end
+
 end
