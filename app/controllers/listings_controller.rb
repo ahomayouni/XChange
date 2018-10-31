c74d7f9cf406682b35bd1e6938d6e4313ccb6c89
Aditya Jain <adi.jain@mail.utoronto.ca>
2018-10-31 03:42:42 -0400

    Fixed an error in listings_ontroller and added show listing functionality

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,12 +4,16 @@ class ListingsController < ApplicationController
     @listing = Listing.new
   end
 
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
   def create
     #render plain: params[:listing].inspect
     @listing = Listing.new(listing_params)
     if @listing.save
       flash[:notice] = "Listing was successfully created"
-      redirect_to listing_show(@listing)
+      redirect_to listing_path(@listing)
     else
       render 'new'
     end
