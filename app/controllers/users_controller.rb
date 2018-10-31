ae912a7a49d456026a24d6db4620193298210b15
ptanugra <p.tanugraha@mail.utoronto.ca>
2018-10-30 21:48:55 -0400

    Added EDIT users view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
   	def user_params_validator
   		params.require(:user).permit(:name, :email,:password,:password_confirmation)
