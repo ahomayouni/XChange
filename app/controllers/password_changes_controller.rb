0d31d7d05975a27ec2b64768c3f4f5f245da5118
ptanugra <p.tanugraha@mail.utoronto.ca>
2018-11-02 22:44:21 -0400

    Added change password functionality

--- app/controllers/password_changes_controller.rb
+++ app/controllers/password_changes_controller.rb
@@ -1,2 +1,18 @@
 class PasswordChangesController < ApplicationController
+	def new
+		@user = User.find(params[:user_id])
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+		@user.password = params[:password_changes][:password]
+		@user.password_confirmation = params[:password_changes][:password_confirmation]
+
+		if @user.save 
+			flash[:success] = "Succeded in changing password!"
+			redirect_to @user 
+		else
+			render 'new'
+		end
+	end
 end
