ac97de61e77929614fbd6a7eeadf1988c07e3172
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-07 18:20:20 -0500

    Added comments to person/ merged subjects

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,15 @@ before_action :find_reply
     
     def create
         @comment = @reply.comments.new comment_params
-        
+        # TODO: add id to comments
+
         # temp message to ensure review save correctly
         if @comment.save
         #redirect_to :back, notice: 'Your review was successfully posted!'
         redirect_back fallback_location: request.referrer, notice: 'Your review was successfully posted!'
         else
-        redirect_back fallback_location: request.referrer, notice: 'Your review was successfully posted!'
-        #redirect_to :back, notice: "Your review wasn't posted!"
+        #redirect_back fallback_location: request.referrer, notice: 'Your review was successfully posted!'
+        redirect_to :back, notice: "Your review wasn't posted!"
         end
     end
     
@@ -27,7 +28,8 @@ before_action :find_reply
     #finds the parent comment if any
     def find_reply
         @reply = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-        @reply = Subject.find_by_id(params[:subject_id]) if params[:subject_id]
+        #@reply = Subject.find_by_id(params[:subject_id]) if params[:subject_id]
+        @reply = Person.find_by_id(params[:person_id]) if params[:person_id]
     end
     
 end
