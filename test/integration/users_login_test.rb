297cea6ac60983b2c50fc037cbaec5954f13fead
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-08 22:27:23 -0500

    Added more testing to login information

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,11 +1,25 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do 
+  test "login with empty information" do 
   	get login_path
   	assert_template 'sessions/new'
   	post login_path, params:{session:{email:"",password:""} }
   	assert_template 'sessions/new'
+  	assert_not flash.empty? #Strain testing on the flash
+  	get root_path
+  	assert flash.empty? #Must be using flash.now to kill the flash after
   end
+
+  test "login with invalid information" do 
+  	get login_path
+  	assert_template 'sessions/new'
+  	post login_path, params:{session:{email:"example",password:"111111"} }
+  	assert_template 'sessions/new'
+  	assert_not flash.empty? 
+  	get root_path
+  	assert flash.empty? 
+  end
+
 
 end
