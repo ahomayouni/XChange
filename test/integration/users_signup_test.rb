e507e8ef83ffd8115b412b37a15cf318f4d99c74
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-08 22:06:26 -0500

    Userlogin invalid params check

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do 
+  test "Invalid signup information" do 
   	get new_user_path
   	assert_no_difference 'User.count' do 
   		post users_path, params:{ user: {name:"",
