3e2acc451fb8ffb6b910192ab379b39b6c41e83e
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-08 22:13:14 -0500

    Added more test for users model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-  	@user = User.new(name: "Jokowi", email: "jokowi@private.com")
+  	@main_subject = User.new(name: "Jokowi", email: "jokowi@private.com")
   end
 
   test "password is not present" do
@@ -10,8 +10,24 @@ class UserTest < ActiveSupport::TestCase
   	assert_not @test_subject.valid?
   end
 
-  test "should not be valid" do
+  test "Fields are all matched up and should be valid " do
   	@test_subject = User.new(name: "Jokowi", email: "jokowi@private.com",password: '111111', password_confirmation:'111111')
   	assert @test_subject.valid?
   end
+
+  test "name should not be empty" do
+  	@test_subject = User.new(name: "  ", email: "jokowi@private.com",password: '111111', password_confirmation:'111111')
+  	assert_not @test_subject.valid?
+  end
+
+  test "password should match" do
+  	@test_subject = User.new(name: "  ", email: "jokowi@private.com",password: '111111', password_confirmation:'000000')
+  	assert_not @test_subject.valid?
+  end
+
+  test "password must be at least 5 characters long" do
+  	@test_subject = User.new(name: "  ", email: "jokowi@private.com",password: '1', password_confirmation:'1')
+  	assert_not @test_subject.valid?
+  end
+
 end
