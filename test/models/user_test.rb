b9c4d33d2d2f217bff68f222af113442ab3afe9a
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-08 22:10:52 -0500

    User Model testing for validity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@user = User.new(name: "Jokowi", email: "jokowi@private.com")
+  end
+
+  test "password is not present" do
+  	@test_subject = User.new(name: "Jokowi", email: "jokowi@private.com")
+  	assert_not @test_subject.valid?
+  end
+
+  test "should not be valid" do
+  	@test_subject = User.new(name: "Jokowi", email: "jokowi@private.com",password: '111111', password_confirmation:'111111')
+  	assert @test_subject.valid?
+  end
 end
