7830c20c7fc6782ebd94d8328d850c85a97e4345
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-09 23:37:56 -0500

    generating review for people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,30 @@
 ## genarate from rails if needed
 class PeopleController < ApplicationController
-
+  before_action :find_person
+  before_action :find_review
     #basic shows initial layout of subject
       def index
         @people = Person.all
       end
-    
+      
+      def update
+        @reviews
+      end
+
       def show
-        @person = Person.find(params[:id])
+        @person
       end 
 
+      private
+
+      def find_person
+        @person = Person.find(params[:id])
+      end
+
+      def find_review
+        @reviews = Comment.where(id: @person.id)
+      end
+
+      
 
 end
