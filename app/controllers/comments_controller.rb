8b880abf8dbf7cf181c2f53719a6b85dc096dfeb
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-10 11:46:19 -0500

    Added listing review

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,11 +9,12 @@ before_action :find_reply
     end
 
     def create
+        #start user with current id
         @comment = @reply.comments.new comment_params
         @comment.commenter_id = current_user.id
         
-        # temp message to ensure review save correctly
         if @comment.save
+            #when users reply to a person
             if @comment.reply_type == "Person"
                 @person = Person.find(@comment.subject_id)
                 if @person.review
@@ -25,6 +26,20 @@ before_action :find_reply
                     @person.write_attribute(:review, old_review)
                 else
                     @person.write_attribute(:review, @person.review)
+                end
+            end
+            #when you comment on listings
+            if @comment.reply_type == "Listing"
+                @listing = Listing.find(listing_id)
+                if @listing.review
+                    old_review = @listing.review
+                    total_reviews = Comment.where(subject_id: @comment.comment_id, reply_type: "Listing").length
+                    old_review *= total_reviews
+                    old_review += rating
+                    old_review /= total_reviews+1
+                    @listing.write_attribute(:review, old_review)
+                else
+                    @listing.write_attribute(:review, @listing.review)
                 end
             end
         #redirect_to :back, notice: 'Your review was successfully posted!'
