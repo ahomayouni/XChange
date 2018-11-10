85f40ccfd28f8891760d2cb5138d823d06a89222
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-09 22:32:03 -0500

    added subjects migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_011827) do
+ActiveRecord::Schema.define(version: 2018_11_10_033134) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_11_10_011827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "commenter_id"
+    t.integer "subject_id"
   end
 
   create_table "groups", force: :cascade do |t|
