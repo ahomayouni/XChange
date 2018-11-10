82bfb44ea97030e8f9dc415f234089b092d8105a
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-09 20:14:43 -0500

    Fixes listing issues/validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
     # one comment can have many comments reply to it
     belongs_to :reply, polymorphic: true
     has_many :comments, as: :reply
+
+    validates :body, length: {minimum: 1}
+
 end
