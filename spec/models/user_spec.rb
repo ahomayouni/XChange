668b808261ae7d1722b7e1e83a68a0c1ed8d83de
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-11 16:54:37 -0500

    Basic User model validation with rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,69 @@ RSpec.describe User, type: :model do
 			user_save_success = @user.save
 			expect(user_save_success).to eq(false)
 		end
+
+		it 'ensures name is presence' do
+			@user = User.new(email: "dragonball@gmail.com",
+				             password:              "dodo",
+				             password_confirmation: "dodo",
+				             activated: true,
+				             activated_at: Time.zone.now,
+				             person: Person.create(
+				              address: Faker::Address.street_address,
+				              phone_number: '6471678732',
+				              description: 'I am a bot created by the master Peter Tanugraha'
+				             ))
+			user_save_success = @user.save
+			expect(user_save_success).to eq(false)
+		end
+
+		it 'ensures password matches' do
+			@user = User.new(name:  "dodo",
+							 email: "dragonball@gmail.com",
+				             password:              "password1",
+				             password_confirmation: "password2",
+				             activated: true,
+				             activated_at: Time.zone.now,
+				             person: Person.create(
+				              address: Faker::Address.street_address,
+				              phone_number: '6471678732',
+				              description: 'I am a bot created by the master Peter Tanugraha'
+				             ))
+			user_save_success = @user.save
+			expect(user_save_success).to eq(false)
+		end
+
+		it 'ensures password has at least 5 different characters although they match' do
+			@user = User.new(name:  "dodo",
+							 email: "dragonball@gmail.com",
+				             password:              "1",
+				             password_confirmation: "1",
+				             activated: true,
+				             activated_at: Time.zone.now,
+				             person: Person.create(
+				              address: Faker::Address.street_address,
+				              phone_number: '6471678732',
+				              description: 'I am a bot created by the master Peter Tanugraha'
+				             ))
+			user_save_success = @user.save
+			expect(user_save_success).to eq(false)
+		end
+
+		it 'ensures user saves correctly, when valid data is present' do
+			@user = User.new(name:  "dodo",
+							 email: "dragonball@gmail.com",
+				             password:              "satuikanasin",
+				             password_confirmation: "satuikanasin",
+				             activated: true,
+				             activated_at: Time.zone.now,
+				             person: Person.create(
+				              address: Faker::Address.street_address,
+				              phone_number: '6471678732',
+				              description: 'I am a bot created by the master Peter Tanugraha'
+				             ))
+			user_save_success = @user.save
+			expect(user_save_success).to eq(true)
+		end
 	end
 
 	context 'scope tests' do
