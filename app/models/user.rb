023e2263122427807923d9022a7a7ab3ee9d0851
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-12 08:57:08 -0500

    Added dependency of user to the notification object and also some more dependency testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-	has_many :notifications, foreign_key: :recipient_id
+	has_many :notifications, foreign_key: :recipient_id, :dependent => :destroy
 
 	has_many :borrow_requests
 
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe Notification, type: :model do
 		  	expect(notif_results).to eq(false)
 	  end
 
-	  it 'simply creating a notification' do 
+	  it 'must not save when the action is empty' do 
 		  	@user = User.new(name:  "dodo",
 							email: "dragonball@gmail.com",
 							password:              "satuikanasin",
@@ -73,5 +73,42 @@ RSpec.describe Notification, type: :model do
 		  	notif_results = @notification.save
 		  	expect(notif_results).to eq(false)
 	  end
+  end
+
+  context 'dependency checking' do 
+  	# Test for dependency here. Such as when create notification for recipient and actor. I should be able to access them
+  	# Also what happens when I delete the actor/recipient? Should the notification be deleted from the database?
+  	it 'should delete the notification when the recipient object has been destroyed' do 
+  		@user = User.new(name:  "dodo",
+							email: "dragonball@gmail.com",
+							password:              "satuikanasin",
+							password_confirmation: "satuikanasin",
+							activated: true,
+							activated_at: Time.zone.now,
+							person: Person.create(
+							address: Faker::Address.street_address,
+							phone_number: '6471678732',
+							description: 'I am a bot created by the master Peter Tanugraha'
+							))
+
+
+		  	@user_2 = User.new(name:  "dodo",
+								email: "dragonball_2@gmail.com",
+							    password:              "satuikanasin",
+							    password_confirmation: "satuikanasin",
+							    activated: true,
+							    activated_at: Time.zone.now,
+							    person: Person.create(
+							    address: Faker::Address.street_address,
+							    phone_number: '6471678732',
+							    description: 'I am a bot created by the master Peter Tanugraha'
+						))
+
+		  	@user.save
+		  	@user_2.save
+		  	Notification.create(recipient: @user_2,actor: @user,action:"testing_purposes",notifiable:@user)
+		  	@user_2.destroy
+		  	expect(Notification.all.count).to eq(0)
+  	end
   end
 end
