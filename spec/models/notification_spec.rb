6e029c05f1573b7635ffb48280330c0f233cd365
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-12 08:58:20 -0500

    Added more dependency testing

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -110,5 +110,38 @@ RSpec.describe Notification, type: :model do
 		  	@user_2.destroy
 		  	expect(Notification.all.count).to eq(0)
   	end
+
+  	it 'should NOT delete the notification when the actor object has been destroyed' do 
+  		@user = User.new(name:  "dodo",
+							email: "dragonball@gmail.com",
+							password:              "satuikanasin",
+							password_confirmation: "satuikanasin",
+							activated: true,
+							activated_at: Time.zone.now,
+							person: Person.create(
+							address: Faker::Address.street_address,
+							phone_number: '6471678732',
+							description: 'I am a bot created by the master Peter Tanugraha'
+							))
+
+
+		  	@user_2 = User.new(name:  "dodo",
+								email: "dragonball_2@gmail.com",
+							    password:              "satuikanasin",
+							    password_confirmation: "satuikanasin",
+							    activated: true,
+							    activated_at: Time.zone.now,
+							    person: Person.create(
+							    address: Faker::Address.street_address,
+							    phone_number: '6471678732',
+							    description: 'I am a bot created by the master Peter Tanugraha'
+						))
+
+		  	@user.save
+		  	@user_2.save
+		  	Notification.create(recipient: @user_2,actor: @user,action:"testing_purposes",notifiable:@user)
+		  	@user.destroy
+		  	expect(Notification.all.count).to eq(1) #the notification should still exist
+  	end
   end
 end
