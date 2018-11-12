6055759176a1601bb4503607ea412186d5b9e1a9
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-12 00:44:16 -0500

    Reinforced the notifications model with validation checks along with the tests

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,6 +3,10 @@ class Notification < ApplicationRecord
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
 
+  validates :recipient_id,presence:true
+  validates :actor_id,presence:true
+  validates :action,presence:true
+
   scope :unread, ->{ where(read_at: nil) }
   scope :recent, ->{ order(created_at: :desc).limit(5) }
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -34,5 +34,44 @@ RSpec.describe Notification, type: :model do
 
 		  	expect(Notification.all.count).to eq(1)
 	  end
+
+	  it 'must not save when recipient or actor is empty' do 
+		  	@notification = Notification.new(action:"testing_purposes",notifiable:@user)
+		  	notif_results = @notification.save
+		  	expect(notif_results).to eq(false)
+	  end
+
+	  it 'simply creating a notification' do 
+		  	@user = User.new(name:  "dodo",
+							email: "dragonball@gmail.com",
+							password:              "satuikanasin",
+							password_confirmation: "satuikanasin",
+							activated: true,
+							activated_at: Time.zone.now,
+							person: Person.create(
+							address: Faker::Address.street_address,
+							phone_number: '6471678732',
+							description: 'I am a bot created by the master Peter Tanugraha'
+							))
+
+
+		  	@user_2 = User.new(name:  "dodo",
+								email: "dragonball_2@gmail.com",
+							    password:              "satuikanasin",
+							    password_confirmation: "satuikanasin",
+							    activated: true,
+							    activated_at: Time.zone.now,
+							    person: Person.create(
+							    address: Faker::Address.street_address,
+							    phone_number: '6471678732',
+							    description: 'I am a bot created by the master Peter Tanugraha'
+						))
+
+		  	@user.save
+		  	@user_2.save
+		  	@notification = Notification.new(recipient: @user_2,actor: @user,notifiable:@user)
+		  	notif_results = @notification.save
+		  	expect(notif_results).to eq(false)
+	  end
   end
 end
