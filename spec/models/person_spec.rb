5f73e4b746bde08f892a40e9c24d3cf95237991f
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-12 00:07:55 -0500

    Added more testing to person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  context "scope tests" do 
+  context "validation tests" do 
   	it 'Must be able to access the person information on the user' do
 	  	@user = User.new(name:  "dodo",
 								 email: "dragonball@gmail.com",
@@ -16,6 +16,30 @@ RSpec.describe Notification, type: :model do
 					             ))
 	  	@person_obj = @user.person 
 	  	expect(@person_obj.nil?).to eq(false)
+  	end
+
+
+  	it 'Must be able to update the person information' do
+	  	@user = User.new(name:  "dodo",
+								 email: "dragonball@gmail.com",
+					             password:              "satuikanasin",
+					             password_confirmation: "satuikanasin",
+					             activated: true,
+					             activated_at: Time.zone.now,
+					             person: Person.create(
+					              address: Faker::Address.street_address,
+					              phone_number: '6471678732',
+					              description: 'I am a bot created by the master Peter Tanugraha'
+					             ))
+	  	@person_obj = @user.person 
+	  	@person_obj.phone_number = '1111111111'
+	  	@person_obj.save
+
+	  	# Refresh the user model we are working with from the database
+	  	@user = User.find(@user.id)
+	  	@new_person_obj = @user.person
+
+	  	expect(@new_person_obj.phone_number).to eq('1111111111')
   	end
   end
 end
