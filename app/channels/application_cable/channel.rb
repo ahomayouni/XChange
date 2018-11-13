8e86f5604f78c80b3c8a66aa3833a8c81e345ebc
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-13 18:02:03 -0500

    added chat room communications

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,12 @@
 module ApplicationCable
-  class Channel < ActionCable::Channel::Base
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.email
+    end
+
+    
   end
 end
