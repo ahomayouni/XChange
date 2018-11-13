a028ac1f18e74be4588e446f66c7420c131280dc
ahomayouni <arash.homayouni@mail.utoronto.ca>
2018-11-13 16:44:21 -0500

    make locations null until fixed locally

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,33 +1,18 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.all
+    @locations = nil
   end
   def show
-    @location = Location.find(params[:id]) #id here refers to user_id
+    @location = nil
   end 
   def new
-    @location = current_user.location.new
+    @location = nil
   end
   def edit
-    if params[:id].present? and params[:lat].present? and params[:long].present? and params[:address].present?
-      @location = Location.find(params[:id])
-      @location.latitude = params[:lat]
-      @location.longitude = params[:long]
-      @location.address = params[:address]
-    end
-    if !@location.save
-      flash[:error] = "Location could not be updated"
-    end
+    @location = nil
   end
   def create
-    @location = current_user.location.create(location_params_validator)
-    
-    if @location.save 
-          flash[:success] = "New Location created"
-          redirect_to location_path(current_user.id)
-        else
-          render 'new'
-        end
+    @location = nil
   end
   def destoy
   end
