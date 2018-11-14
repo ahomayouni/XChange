fb776d21869b64e80e5e24138b80119feef6fa22
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-13 21:18:17 -0500

    action cable fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [ 'https://xchange-csc444.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://xchange-csc444.herokuapp.com', 'http://xchange-csc444.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
