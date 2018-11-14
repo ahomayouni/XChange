509bb0dacd11c35805a935b7ddb43dd214bfd3ba
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-13 22:34:06 -0500

    reverting changes

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,13 +1,5 @@
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_verified_user
-      # logs for debugging
-      logger.add_tags 'ActionCable', current_user.email
-    end
-
+  class Channel < ActionCable::Channel::Base
     
   end
 end
