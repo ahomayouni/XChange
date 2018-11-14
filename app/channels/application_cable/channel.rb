13350caa93b72e11bee9638a3fb29099c8ac6345
ahomayouni <arash.homayouni@mail.utoronto.ca>
2018-11-13 21:48:30 -0500

    modified channel.rb

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,13 +1,8 @@
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_verified_user
-      # logs for debugging
-      logger.add_tags 'ActionCable', current_user.email
-    end
-
-    
-  end
+	class Channel < ActionCable::Channel::Base
+  	end
 end
+
+
+
+
