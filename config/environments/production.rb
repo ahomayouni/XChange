20d3a408085fb19b8d34931746345206e33f6146
ahomayouni <arash.homayouni@mail.utoronto.ca>
2018-11-13 21:10:45 -0500

    possible fix: adding actioncable in config paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://xchange-csc444.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://xchange-csc444.herokuapp.com', 'http://xchange-csc444.herokuapp.com']
 end
