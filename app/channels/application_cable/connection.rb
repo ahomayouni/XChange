1b0635c9f9c19d90082864a8aebce7d8154b0c1c
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-13 20:48:44 -0500

    actionCable fix

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
+# app/channels/application_cable/connection.rb
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = current_user
+    end
+
   end
 end
