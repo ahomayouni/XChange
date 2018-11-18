8a0931b2cd347ada67e3f264ca438fd6dea8a6f6
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-18 03:17:47 -0500

    added message migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_081420) do
+ActiveRecord::Schema.define(version: 2018_11_18_081633) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -103,6 +103,12 @@ ActiveRecord::Schema.define(version: 2018_11_18_081420) do
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_memberships_on_group_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notifications", force: :cascade do |t|
