19716488f5a15b3488cbe5d400c87f4aa239e87d
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-18 03:45:08 -0500

    chatroom typo fix

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,7 +2,7 @@ class Chatroom < ApplicationRecord
     # destroy all messages when chatroom is destroyed
     has_many :messages, dependent: :destroy
     # allows Chatroom to look up each user through which message they send
-    has_many :users, through :messages
+    has_many :users, through: :messages
     validates :topic
 
 end
