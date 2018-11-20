12e5946e7e62bddb349db48d4d4e3b9e03afbe78
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-19 22:52:31 -0500

    added inital message view/controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+
+    def create
+      message = Message.new(message_params)
+      message.user = current_user
+      if message.save
+        # do stuff
+      else 
+        redirect_to chatrooms_path
+      end
+    end
+  
+    private
+    def message_params
+    params.require(:message).permit(:content, :chatroom_id)
+    end
+end
