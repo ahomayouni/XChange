03c9b373f64d719c45b526879c79d92bb7e2a9cb
Aditya Jain <adi.jain@mail.utoronto.ca>
2018-11-21 16:47:37 -0500

    Added basic validations

--- app/models/live_search.rb
+++ app/models/live_search.rb
@@ -1,3 +1,11 @@
 class LiveSearch < ApplicationRecord
   belongs_to :user
+  validates :category, :title, :from_when, :to_when, :where, presence: true
+  validate :validateTimings
+
+  def validateTimings
+    if self.from_when && self.to_when && self.from_when > self.to_when
+      errors[:base] << "Start Time must be before End Time"
+    end
+  end
 end
