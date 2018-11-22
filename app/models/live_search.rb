7a8a236242ee3e2a54a38430c6e03e3f20e9c8c2
Aditya Jain <adi.jain@mail.utoronto.ca>
2018-11-21 19:43:05 -0500

    Added validations

--- app/models/live_search.rb
+++ app/models/live_search.rb
@@ -1,10 +1,10 @@
 class LiveSearch < ApplicationRecord
   belongs_to :user
-  # validates :category, presence: true
-  # validates :title, presence: true
-  # validates :from_when, presence: true
-  # validates :to_when, presence: true
-  # validates :where, presence: true
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :from_when, presence: true
+  validates :to_when, presence: true
+  validates :where, presence: true
   validate :validateTimings
 
   def validateTimings
