cdc145bedc2524fd9b5fa6973d3194bb4cdd2a02
marujeyakumar <maru.jeyakumar@mail.utoronto.ca>
2018-11-23 23:18:56 -0500

    Simple implementation for viewing someone else's profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+    def is_current_user
+        @user = User.find(params[:id])
+        if @user.id == current_user.id
+            return true 
+        else 
+            return false 
+        end 
+    end 
 end
