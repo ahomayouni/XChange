493ed8af69c5b6cf9ecf8f98fdad6e64c4d75f72
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-23 23:48:42 -0500

    fix

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -12,7 +12,7 @@ class ChatroomsController < ApplicationController
   private
   def chatroom_params
     #don't need title as title can be set optionally by user
-    params.require(:chatroom).permit(:title,:user_ids => [])
+    params.require(:chatroom).permit(:title)
   end
 
 end
