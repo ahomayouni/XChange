2ffe71344f0192146e74e9d0992b7ab153cee80a
vermaarn <arnav.verma@mail.utoronto.ca>
2018-11-23 23:45:36 -0500

    starting to merge chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,18 @@
 class ChatroomsController < ApplicationController
-  
-    def show
-      @chatroom = Chatroom.find_by(slug: params[:slug])
-      @message = Message.new
-    end
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new chatroom_params
+    
+  end
+    
+
+  private
+  def chatroom_params
+    #don't need title as title can be set optionally by user
+    params.require(:chatroom).permit(:title, ,:user_ids => [])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_200352) do
+ActiveRecord::Schema.define(version: 2018_11_24_044152) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_200352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chatroom_id"
+    t.integer "user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
