e50640451094cf2503f33ab4baf4141837462e60
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-26 16:49:52 -0500

    Template for group spec

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Model Validation Tests" do
+  	it "should create a group successfully when all the valid parameters are given" do 
+  		@group = Group.new 
+  		@group.name = "People of North York"
+  		@group.description = "People living in the north york area looking to borrow and lend stuff"
+  		@group.isPublic = true 
+  		@group.save
+
+  		expect(Group.all.count).to eq(1)
+  		
+  	end
+
+  end
 end
