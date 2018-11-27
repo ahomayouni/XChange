73f5bae674c1a64a9e9910cdfe4a161df3a27b73
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-27 10:13:00 -0500

    Initial Capybara

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -10,7 +10,14 @@ RSpec.feature "Users", type: :feature do
 				expect(page).to have_content('Email')
 				expect(page).to have_content('Password')
 				expect(page).to have_content('Confirm Password')
+
+				fill_in 'user_name', with: 'Peter Tanugraha'
+				fill_in 'user_email', with: 'p.tar@gmail.com'
+				fill_in 'user_password', with: '111111'
+				fill_in 'user_password_confirmation', with: '111111'
+				click_button 'Submit'
 			end
+			expect(page).to have_content('Thanks for signing up with XChange!')
 		end
 
 		it 'should fail' do 
