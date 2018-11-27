9316561e248d56fc0afadce139315ebd134bf77d
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-26 21:19:06 -0500

    Added more spec testing on groups controller

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -7,7 +7,6 @@ end
 
 RSpec.describe GroupsController, type: :controller do
 
-
 	describe 'GET #index' do 
 		context 'GET #index without logged in' do
 			it 'must not be able to index notification when user is not logged in ' do
@@ -32,6 +31,43 @@ RSpec.describe GroupsController, type: :controller do
 				login(user)
 
 				get :index
+				expect(response).to be_successful
+			end
+		end
+	end
+
+	describe 'POST #show' do 
+		context "POST #show without logged in" do
+			it "Must not be able to show when user is not logged in " do 
+				post :show, params: {id:1}
+				expect(response).to_not be_successful
+			end
+		end
+
+		context "POST #show when logged in" do
+			before(:each) do 
+				user = User.create!(name:  "dodo",
+								 email: "dragonball@gmail.com",
+					             password:              "satuikanasin",
+					             password_confirmation: "satuikanasin",
+					             activated: true,
+					             activated_at: Time.zone.now,
+					             person: Person.create(
+					              address: Faker::Address.street_address,
+					              phone_number: '6471678732',
+					              description: 'I am a bot created by the master Peter Tanugraha'
+					             ))
+				login(user)
+
+				@group = Group.new 
+		  		@group.name = "People of North York"
+		  		@group.description = "People living in the north york area looking to borrow and lend stuff"
+		  		@group.isPublic = true 
+		  		@group.save
+
+			end
+			it " Successfull response when logged in" do 
+				post :show, params: {id:1}
 				expect(response).to be_successful
 			end
 		end
@@ -72,14 +108,17 @@ RSpec.describe GroupsController, type: :controller do
 
 				expect(Group.all.count).to eq(1)
 				@group = Group.first 
-				puts @group.name
 				expect(response).to redirect_to(group_path(@group))
-				# expect(response).to redirect_to(root_path)
-				# expect(response).to be_successful
 			end
 
 			it "will be render new when INVALID params are given" do 
-				# To be done
+				post :create, params: {group:{ name:  "People of North York",
+				             isPublic: true,
+				             owner_id: @user.id}
+				         }
+
+				expect(Group.all.count).to eq(0) 
+				expect(response).to render_template('new')
 			end
 		end
 	end
