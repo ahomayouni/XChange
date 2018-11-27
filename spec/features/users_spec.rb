a78bc9e15a3bc3c7aaebdbad45e442189497ff05
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-27 00:50:55 -0500

    Template for capybaraaaaaaa

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.feature "Users", type: :feature do
 	context 'create new user' do 
 		it 'should be successful' do 
+			visit root_path
+			within('#signupModal') do 
+				expect(page).to have_content('Sign up')
+				expect(page).to have_content('Name')
+				expect(page).to have_content('Email')
+				expect(page).to have_content('Password')
+				expect(page).to have_content('Confirm Password')
+			end
 		end
 
 		it 'should fail' do 
