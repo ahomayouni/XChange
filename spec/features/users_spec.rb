16512aab4c2c3be4dca7672c70dd228337f2522e
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-27 00:40:52 -0500

    Initialize capybara

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+	context 'create new user' do 
+		it 'should be successful' do 
+		end
+
+		it 'should fail' do 
+		end
+	end
+end
