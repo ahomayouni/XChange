6d7e4774bf54ce12ec65ca5e9debf3ab40be36ed
ahomayouni <arash.homayouni@mail.utoronto.ca>
2018-11-28 12:50:28 -0500

    compile image assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,4 +106,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.compile = true #for heroku images
+  config.assets.digest = true
 end
