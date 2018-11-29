3f684fc0b5803958143229801a68c2c2e6fd8c56
Peter Tanugraha <p.tanugraha@mail.utoronto.ca>
2018-11-29 11:27:43 -0500

    Removed start-end validation date in listing. Removed start date picker range to simulate a late listing request. Added a status change in cron job and also added a late enum attribute in the model of borrowrequest

--- app/models/borrow_request.rb
+++ app/models/borrow_request.rb
@@ -1,6 +1,6 @@
 class BorrowRequest < ApplicationRecord
   attr_accessor
-  enum status: [:requested, :approved, :declined, :borrowed, :returned]
+  enum status: [:requested, :approved, :declined, :borrowed, :returned, :late]
   belongs_to :listing
   belongs_to :user
   has_many :notifications
