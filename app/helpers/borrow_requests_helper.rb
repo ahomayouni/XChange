8420ac02a0b70f40f5b256a3edc545e2bdc1a7d1
marujeyakumar <maru.jeyakumar@mail.utoronto.ca>
2018-11-29 00:19:00 -0500

    Fix for the pill colours, they change upon Status change now

--- app/helpers/borrow_requests_helper.rb
+++ app/helpers/borrow_requests_helper.rb
@@ -1,9 +1,15 @@
 module BorrowRequestsHelper 
     def get_pill_color_from_status(status)
         if status.camelize == "Approved" 
-            return "green"
-        else 
-            return ""
+            return "success"
+        elsif status.camelize=="Requested" 
+            return "info"
+        elsif status.camelize=="Rejected"
+            return "danger"
+        elsif status.camelize=="Borrowed"
+            return "primary"
+        elsif status.camelize=="Returned"
+            return "dark"
         end 
     end 
 end 
