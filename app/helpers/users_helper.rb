62a16ac84e92ff8cfc5de35aedc9327f23337427
Aditya Jain <adi.jain@mail.utoronto.ca>
2018-11-29 00:09:27 -0500

    conditional rendering for CRUD functions for borrow request

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,18 +2,18 @@ module UsersHelper
     def is_current_user
         @user = User.find(params[:id])
         if @user.id == current_user.id
-            return true 
-        else 
-            return false 
-        end 
-    end 
-    def get_rounded_user_rating 
-        @user = User.find(params[:id])
-        if @user.person.rating.nil?
-            return 0
+            return true
         else
-            return @user.person.rating.round
-        end 
-    end 
+            return false
+        end
+    end
 
+    def get_rounded_user_rating
+       @user = User.find(params[:id])
+       if @user.person.rating.nil?
+           return 0
+       else
+           return @user.person.rating.round
+       end
+   end 
 end
